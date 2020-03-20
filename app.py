e5c70b966fdb3db470b1ad50d8b96f92a4b87dfc
Rafaela Becerra <rafaelabecerrarobalino@gmail.com>
2020-03-20 12:41:10 +0100

    Add historical data

--- app.py
+++ app.py
@@ -6,10 +6,20 @@ import dash_html_components as html
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 import pandas as pd
-url="https://datos.madrid.es/egob/catalogo/300228-21-accidentes-trafico-detalle.csv"
-data = pd.read_csv(url, sep=";", encoding="latin1")
-data.head()
+url_1="https://datos.madrid.es/egob/catalogo/300228-21-accidentes-trafico-detalle.csv"
+data_1 = pd.read_csv(url_1, sep=";", encoding="latin1")
+data_1.head()
+print(data_1)
 
+url_2="https://datos.madrid.es/egob/catalogo/300228-19-accidentes-trafico-detalle.csv"
+data_2 = pd.read_csv(url_2, sep=";", encoding="latin1")
+data_2.drop(data_2.columns[len(data_2.columns)-1], axis=1, inplace=True)
+data_2.head()
+print(data_2)
+
+#Create only one data frame
+data_1 = data_1.append(pd.DataFrame(data = data_2), ignore_index=True)
+data1=data_1
 #import os
 #c=pd.read_csv("2020_Accidentalidad.csv", sep=";", encoding="latin1" )
 #c.head()
@@ -43,9 +53,9 @@ data1['ADDRESS'] = data1['ADDRESS'].str.replace('.*\\. ','')
 
 #Change levels of injury based on dataset dictionary
 data1['INJURY'] ="Mild"
-data1.loc[2,]
+#data1.loc[2,]
 #data1.loc[data1.LESIVIDAD==3,'INJURY']
-data1.dtypes
+#data1.dtypes
 data1.loc[data1.LESIVIDAD==3,'INJURY']= 'Fatal'
 data1.loc[data1['LESIVIDAD']==14,'INJURY']= 'Without assistance'
 data1.loc[data1.LESIVIDAD==70,'INJURY']= 'Unknown'
@@ -55,6 +65,15 @@ data1.loc[data1.LESIVIDAD==70,'INJURY']= 'Unknown'
 data1['FECHA'] = pd.to_datetime(data1['FECHA'])
 data1['DAY'] = data1['FECHA'].dt.strftime('%A')
 data1['FECHA'] = data1['FECHA'].dt.strftime('%d/%m/%Y')
+data1.dtypes
+
+#Create historical data
+#Create data outputs
+#Min data
+#Total accidents 2020
+#Total victims
+#Total fatal victims
+
 
 app.layout = html.Div(children=[
     html.H1(children='Hello Dash'),
