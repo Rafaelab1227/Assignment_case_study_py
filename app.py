b3cf459040a77796f4705ef7a9bd56d19f4992cd
Rafaela Becerra <rafaelabecerrarobalino@gmail.com>
2020-03-20 12:14:57 +0100

    Edit address column and change type of variables

--- app.py
+++ app.py
@@ -10,10 +10,10 @@ url="https://datos.madrid.es/egob/catalogo/300228-21-accidentes-trafico-detalle.
 data = pd.read_csv(url, sep=";", encoding="latin1")
 data.head()
 
-import os
-c=pd.read_csv("2020_Accidentalidad.csv", sep=";", encoding="latin1" )
-c.head()
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+#import os
+#c=pd.read_csv("2020_Accidentalidad.csv", sep=";", encoding="latin1" )
+#c.head()
+#app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 # Changes in dataset ------------------------------------------------------
 # Changes in column names
@@ -22,32 +22,39 @@ data1 = data.rename(columns={"NÚMERO":"NUMERO","ESTADO METEREOLÓGICO":"ESTADO.
         "TIPO ACCIDENTE":"TIPO.ACCIDENTE","TIPO PERSONA":"TIPO.PERSONA","RANGO DE EDAD":"RANGO.DE.EDAD"})
 list(data1.columns)
 
+#Create column of address
 data1['ADDRESS'] =data1.apply(lambda x: '.'.join([x['CALLE'],', ',x['NUMERO'],', ','MADRID, SPAIN']),axis=1)
 list(data1.columns)
 data1.head()
 data1.describe()
 print(data1)
 
-#data1 =data1.assign(is_senior = lambda dataframe: dataframe['CALLE','NUMERO'].map(lambda name: CALLE+", "+NUMERO))
+#Replaces needed in address
 data1['ADDRESS'] = data1['ADDRESS'].str.replace('\.','')
-data1.loc[1,'ADDRESS']
+#data1.loc[1,'ADDRESS']
 data1['ADDRESS'] = data1['ADDRESS'].str.replace(', -, ',', 0,')
 data1['ADDRESS'] = data1['ADDRESS'].str.replace(' NA, ','')
 data1 = data1.fillna('Unknown')
 
-
+#Eliminate all the names ("CALL.", "AV.", etc) that are not supported by geolocation & eliminate double direction.
+data1['ADDRESS'] = data1['ADDRESS'].str.replace('.*\\/','')
+data1['ADDRESS'] = data1['ADDRESS'].str.replace('.*\\. ','')
+#data1.loc[105,]
 
 #Change levels of injury based on dataset dictionary
-data$INJURY <- "Mild"
-data[is.na(data$LESIVIDAD),match("LESIVIDAD",names(data))]<- "Unknown"
-data[is.na(data$LESIVIDAD), match("INJURY",names(data))] <- "Unknown"
-data[data$LESIVIDAD=="3", match("INJURY",names(data))] <- "Fatal"
-data[data$LESIVIDAD=="14"|data$LESIVIDAD=="", match("INJURY",names(data))] <- "Without assistance"
-data[data$LESIVIDAD=="77", match("INJURY",names(data))] <- "Unknown"
+data1['INJURY'] ="Mild"
+data1.loc[2,]
+#data1.loc[data1.LESIVIDAD==3,'INJURY']
+data1.dtypes
+data1.loc[data1.LESIVIDAD==3,'INJURY']= 'Fatal'
+data1.loc[data1['LESIVIDAD']==14,'INJURY']= 'Without assistance'
+data1.loc[data1.LESIVIDAD==70,'INJURY']= 'Unknown'
 
-
-
-
+#Change type of variables
+#data1 = data1.astype({'FECHA': 'float64', 'HORA': 'object'})
+data1['FECHA'] = pd.to_datetime(data1['FECHA'])
+data1['DAY'] = data1['FECHA'].dt.strftime('%A')
+data1['FECHA'] = data1['FECHA'].dt.strftime('%d/%m/%Y')
 
 app.layout = html.Div(children=[
     html.H1(children='Hello Dash'),
